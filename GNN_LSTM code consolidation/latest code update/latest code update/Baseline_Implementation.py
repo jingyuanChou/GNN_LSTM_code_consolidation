37b5a9c3cc937cd9040030652401455f5a1824fe
JingyuanC1111 <jc2wv@virginia.edu>
2023-09-29 17:53:17 -0400

    code update

--- GNN_LSTM code consolidation/latest code update/latest code update/Baseline_Implementation.py
+++ GNN_LSTM code consolidation/latest code update/latest code update/Baseline_Implementation.py
@@ -1,72 +1,13 @@
-import argparse
-
 import torch
-import torch.nn as nn
 import torch.optim as optim
+from sklearn.preprocessing import MinMaxScaler
+from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import networkx as nx
-from sklearn.preprocessing import MinMaxScaler
-from torch_geometric_temporal import DCRNN
+from statsmodels.tsa.api import VAR
 
-from weigth_sage import WeightedSAGEConv
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--device',type=str,default='cpu',help='')
-parser.add_argument('--data',type=str,default='data/METR-LA',help='data path')
-parser.add_argument('--lr',type=float,default=0.01,help='learning rate')
-parser.add_argument('--adj_data', type=str,default='data/sensor_graph/adj_mx.pkl',help='adj data path')
-parser.add_argument('--horizon', type=int, default=4)
-parser.add_argument('--prediction_window', type=int, default=4)
-parser.add_argument('--step_size',type=int,default=100,help='step_size')
-parser.add_argument('--save', type=str, default='model/model.pt',
-                    help='path to save the final model')
-parser.add_argument('--gcn_depth',type=int,default=1,help='graph convolution depth')
-parser.add_argument('--num_nodes',type=int,default=49,help='number of nodes/variables')
-parser.add_argument('--dropout',type=float,default=0.2,help='dropout rate')
-parser.add_argument('--subgraph_size',type=int,default=20,help='k')
-parser.add_argument('--node_dim',type=int,default=16,help='dim of nodes')
-parser.add_argument('--init_embedding_dim',type=int,default=49,help='dim of embedding')
-parser.add_argument('--dilation_exponential',type=int,default=1,help='dilation exponential')
-parser.add_argument('--optim', type=str, default='adam')
-parser.add_argument('--conv_channels',type=int,default=4,help='convolution channels')
-parser.add_argument('--residual_channels',type=int,default=4,help='residual channels')
-parser.add_argument('--skip_channels',type=int,default=8,help='skip channels')
-parser.add_argument('--end_channels',type=int,default=16,help='end channels')
-parser.add_argument('--in_dim',type=int,default=1,help='inputs dimension')
-parser.add_argument('--seq_in_len',type=int,default=12,help='input sequence length')
-parser.add_argument('--seq_out_len',type=int,default=1,help='output sequence length')
-parser.add_argument('--layers',type=int,default=3,help='number of layers')
-parser.add_argument('--batch_size',type=int,default=100,help='batch size')
-parser.add_argument('--learning_rate',type=float,default=0.005,help='learning rate')
-parser.add_argument('--weight_decay',type=float,default=0.0001,help='weight decay rate')
-parser.add_argument('--clip',type=int,default=5,help='clip')
-parser.add_argument('--step_size1',type=int,default=2500,help='step_size')
-parser.add_argument('--step_size2',type=int,default=100,help='step_size')
-parser.add_argument('--L1Loss', type=bool, default=True)
-parser.add_argument('--epochs',type=int,default=200,help='')
-parser.add_argument('--print_every',type=int,default=50,help='')
-parser.add_argument('--seed',type=int,default=101,help='random seed')
-parser.add_argument('--expid',type=int,default=1,help='experiment id')
-parser.add_argument('--propalpha',type=float,default=0.05,help='prop alpha')
-parser.add_argument('--tanhalpha',type=float,default=1,help='adj alpha')
-parser.add_argument('--num_split',type=int,default=1,help='number of splits for graphs')
-parser.add_argument('--normalize', type=int, default=0)
-parser.add_argument('--runs',type=int,default=10,help='number of runs')
-parser.add_argument('--autoregressive_retrain', type = bool, default=True, help='autoregressive training or not')
-parser.add_argument('--multivariate_TE_enhanced', type = bool, default=True, help='Multivariate Transfer Entropy')
-parser.add_argument('--pure_data_driven', type = bool, default=True, help='Use purely data driven for adaptive learning')
-parser.add_argument('--BivariateTE', type = bool, default=True, help='Using Bivaraite Transfer Entropy')
-parser.add_argument('--Plot_4_smaller_regions', type = bool, default=False, help='Plot for smaller regions')
-parser.add_argument('--Vector_Autoregression', type = bool, default=False, help='Perform VAR baseline')
-parser.add_argument('--train_horizon',  type=int, default=1, help = 'specify the horizon for training stage')
-
-args = parser.parse_args()
-
-
-# Assuming data is a numpy array of shape (119, 51)
 def create_dataset(data, input_length, output_length):
     X, y = [], []
     for i in range(len(data) - input_length - output_length + 1):
@@ -77,25 +18,23 @@ def create_dataset(data, input_length, output_length):
 def generate_sequences(data, input_seq_len, pred_seq_len):
     input_sequences = []
     target_sequences = []
-    for t in range(data.shape[1] - input_seq_len - pred_seq_len + 1):
-        input_seq = data[:, t:t+input_seq_len, :]
-        target_seq = data[:, t+input_seq_len:t+input_seq_len+pred_seq_len, 0]
+    for t in range(data.shape[0] - input_seq_len - pred_seq_len + 1):
+        input_seq = data[t:t + input_seq_len,:, :]
+        target_seq = data[t + input_seq_len:t + input_seq_len + pred_seq_len,:, 0]
         input_sequences.append(input_seq)
         target_sequences.append(target_seq)
-    return torch.stack(input_sequences), torch.stack(target_sequences)
-
-
+    return torch.tensor(input_sequences, dtype=torch.float32), torch.tensor(target_sequences, dtype=torch.float32)
 # Define the model
 class LSTMModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(LSTMModel, self).__init__()
         self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True)
         self.fc = nn.Linear(hidden_dim, output_dim)
+
     def forward(self, x):
         h_n, _ = self.lstm(x)
         out = self.fc(h_n[:, -1, :])
         return out.view(x.size(0), 4, -1)
-
 class GRUModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(GRUModel, self).__init__()
@@ -106,36 +45,11 @@ class GRUModel(nn.Module):
         h_n, _ = self.gru(x)
         out = self.fc(h_n[:, -1, :])
         return out.view(x.size(0), 4, -1)
-
-
-class TrafficPredictor(nn.Module):
-    def __init__(self, in_channels, out_channels, K):
-        super(TrafficPredictor, self).__init__()
-        self.dcrnn = DCRNN(in_channels, out_channels, K)
-        self.linear = nn.Linear(out_channels, 1)
-
-    def forward(self, x_sequence, edge_index, edge_weight_sequence, h=None):
-        predictions = []
-
-        # Process the input sequence
-        for t in range(12):
-            x_t = x_sequence[:, t, :]
-            edge_weight_t = edge_weight_sequence[t]
-            h = self.dcrnn(x_t, edge_index, edge_weight_t, h)
-
-        # Predict for the next timestamps
-        for _ in range(4):
-            h = self.dcrnn(h, edge_index, edge_weight_t, h)  # using the last edge_weight_t for simplicity
-            out = self.linear(h)
-            predictions.append(out.unsqueeze(1))
-
-        return torch.cat(predictions, dim=1)
-
-def Vector_Autoregression(data, past, horizon):
+def Vector_Autoregression(data, past, horizon, index_2_fips, fips_2_state):
     # VAR
     # Assuming 'data' is a pandas DataFrame with your 51 variables and 119 timestamps
-    data_x = data[:-4,:]
-    data_y = data[-4:,:]
+    data_x = data[:-4, :]
+    data_y = data[-4:, :]
     model = VAR(data_x)
     fit_model = model.fit(maxlags=past)  # Choose an appropriate lag
     forecast = fit_model.forecast(data[-fit_model.k_ar:], steps=horizon)
@@ -145,7 +59,13 @@ def Vector_Autoregression(data, past, horizon):
 
     list_mae_VAR = list()
     list_of_mape_VAR = list()
+
+    VAR_MAE_dict = dict()
+
     for i in range(forecast.shape[0]):
+        fips = index_2_fips[i]
+        cur_state = fips_2_state[fips]
+        VAR_MAE_dict[cur_state] = sum(abs(forecast[i] - gt[i]))/4
         list_mae_VAR.append(sum(abs(forecast[i] - gt[i])))
         sum_mape = 0
         for j, ele in enumerate(forecast[i]):
@@ -156,16 +76,13 @@ def Vector_Autoregression(data, past, horizon):
     gt = np.sum(gt, axis=0)
     mean_mape_overall = np.sum(list_mae_VAR) / np.sum(gt)
 
-    return forecast
-
-
-
-def LSTM(hopsitalization, input_length, output_length, hidden_dim,index_2_fips, fips_2_state):
+    return VAR_MAE_dict
+def LSTM(hopsitalization, input_length, output_length, hidden_dim, index_2_fips, fips_2_state):
     scaler = MinMaxScaler()
     hopsitalization = scaler.fit_transform(hopsitalization)
 
-    input_dim = hopsitalization.shape[1] # 49
-    output_dim = output_length * input_dim # 4 * 49
+    input_dim = hopsitalization.shape[1]  # 49
+    output_dim = output_length * input_dim  # 4 * 49
 
     length = hopsitalization.shape[0]
     train_idx = int(length * 0.8)
@@ -241,10 +158,9 @@ def LSTM(hopsitalization, input_length, output_length, hidden_dim,index_2_fips, 
     for i in range(loss_of_each_state.shape[1]):
         cur_fips = index_2_fips[i]
         cur_state = fips_2_state[cur_fips]
-        LSTM_result[cur_state] = torch.sum(loss_of_each_state[:,i]) / 4
+        LSTM_result[cur_state] = torch.sum(loss_of_each_state[:, i]) / 4
     return LSTM_result
-
-def GRU(hopsitalization, input_length, output_length, hidden_dim,index_2_fips, fips_2_state):
+def GRU(hopsitalization, input_length, output_length, hidden_dim, index_2_fips, fips_2_state):
     scaler = MinMaxScaler()
     hopsitalization = scaler.fit_transform(hopsitalization)
 
@@ -327,73 +243,12 @@ def GRU(hopsitalization, input_length, output_length, hidden_dim,index_2_fips, f
         cur_state = fips_2_state[cur_fips]
         GRU_result[cur_state] = torch.sum(loss_of_each_state[:, i]) / 4
     return GRU_result
-
-def DCRNNmodel(hopsitalization, hidden_dim, edge_index):
-    scaler = MinMaxScaler()
-    hopsitalization = scaler.fit_transform(hopsitalization)
-
-    node_features = 1
-    num_nodes = 49
-
-    length = hopsitalization.shape[0]
-    train_idx = int(length * 0.8)
-    val_idx = length - 4
-    # Split data into training, validation, and test sets
-    # [0-97] as training data, 98 timestamps, [98:118] as validation data, 21 timestamps , [102,122] total 21 timestamps
-    hopsitalization = np.expand_dims(hopsitalization, -1)
-    train_data = hopsitalization[:train_idx, :]
-    val_data = hopsitalization[train_idx:val_idx, :]
-    test_data = hopsitalization[(train_idx + 4):, :]
-
-    X_train, y_train = generate_sequences(train_data, input_length, output_length)
-    X_val, y_val = generate_sequences(val_data, input_length, output_length)
-    X_test, y_test = generate_sequences(test_data, input_length, output_length)
-
-    # Hyperparameters
-    learning_rate = 0.001
-    out_channels = 16
-    filter_size = 2
-    epochs = 10
-
-    model = TrafficPredictor(1, out_channels, filter_size)
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    criterion = nn.MSELoss()
-
-    for epoch in range(epochs):
-        model.train()
-
-        # Training Loop
-        train_losses = []
-        for i in range(len(X_train)):
-            optimizer.zero_grad()
-            predictions = model(X_train[i], edge_index)
-            loss = criterion(predictions, y_train[i])
-            loss.backward()
-            optimizer.step()
-            train_losses.append(loss.item())
-
-        # Validation Loop
-        model.eval()
-        valid_losses = []
-        with torch.no_grad():
-            for i in range(len(X_val)):
-                predictions = model(X_val[i], edge_index)
-                loss = criterion(predictions, y_val[i])
-                valid_losses.append(loss.item())
-
-        print(
-            f"Epoch {epoch + 1}/{epochs}, Training Loss: {sum(train_losses) / len(train_losses):.4f}, Validation Loss: {sum(valid_losses) / len(valid_losses):.4f}")
-
-
-
-
-def VAR():
-    forecast = Vector_Autoregression()
-
-
-
+def VAR_multi(data, past, horizon, index_2_fips, fips_2_state):
+    forecast = Vector_Autoregression(data, past, horizon,index_2_fips, fips_2_state)
+    return forecast
 
 if __name__ == '__main__':
+
     hopsitalization = pd.read_csv('hosp_weekly_filt_case_data.csv')
     hopsitalization = hopsitalization.reset_index()
     hopsitalization['state'] = hopsitalization['state'].astype(int)
@@ -402,7 +257,7 @@ if __name__ == '__main__':
     fips_2_index = hopsitalization.set_index('state')['index'].to_dict()
     index_2_fips = hopsitalization.set_index('index')['state'].to_dict()
 
-    neighbouring_states = pd.read_csv('neighbors-states.csv')
+    neighbouring_states = pd.read_csv('neighbouring_states.csv')
     states_code = pd.read_csv('state_hhs_map.csv')
 
     neighbouring_states_copy = neighbouring_states[['NeighborStateCode', 'StateCode']]
@@ -426,22 +281,18 @@ if __name__ == '__main__':
 
     hopsitalization = hopsitalization.iloc[:, 30:]  # remove all 0 datapoints
     hopsitalization = hopsitalization.T.values
-    models = ['GRAPHLSTM']
+    models = ['LSTM','GRU','VAR'] # GraphLSTM is in another repository, DCRNN as well
 
     input_length = 12
     output_length = 4
     hidden_dim = 8
+
+    neighbouring_states_edges = neighbouring_states.iloc[0:109, :]
 
     for model in models:
         if model == 'LSTM':
             lstm_mae_result = LSTM(hopsitalization, input_length, output_length, hidden_dim, index_2_fips, fips_2_state)
         elif model == 'GRU':
             gru_mae_result = GRU(hopsitalization, input_length, output_length, hidden_dim, index_2_fips, fips_2_state)
-        elif model == 'DCRNN':
-            edge_index = torch.from_numpy(neighbouring_states.values.T)
-            DCRNNmodel(hopsitalization, hidden_dim, edge_index)
-        else:
-            VAR()
-
-
-
+        elif model == 'VAR':
+            VAR_MAE_RESULT = VAR_multi(hopsitalization, input_length, output_length, index_2_fips, fips_2_state)
---
+++ GNN_LSTM code consolidation/latest code update/latest code update/DCRNN_BASELINE.py
@@ -0,0 +1,169 @@
+import networkx as nx
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from sklearn.preprocessing import MinMaxScaler
+from torch_geometric.utils import degree, to_dense_adj
+from torch_geometric.utils.convert import from_networkx
+
+
+
+def generate_sequences(data, input_seq_len, pred_seq_len):
+    input_sequences = []
+    target_sequences = []
+    for t in range(data.shape[0] - input_seq_len - pred_seq_len + 1):
+        input_seq = data[t:t + input_seq_len,:, :]
+        target_seq = data[t + input_seq_len:t + input_seq_len + pred_seq_len,:, 0]
+        input_sequences.append(input_seq)
+        target_sequences.append(target_seq)
+    return torch.tensor(input_sequences, dtype=torch.float32), torch.tensor(target_sequences, dtype=torch.float32)
+
+
+# DCRNN Components
+class DCRNNCell(nn.Module):
+    def __init__(self, node_features, hidden_channels, K):
+        super(DCRNNCell, self).__init__()
+        self.dc_initial = nn.Linear((K+1) * node_features, hidden_channels)
+        self.dc_hidden = nn.Linear((K+1) * hidden_channels, hidden_channels)
+
+    def forward(self, x, edge_index, h=None):
+        L = normalized_laplacian(edge_index, x.size(0))
+        x_list = [x]
+        for k in range(K):
+            x = torch.matmul(L, x)
+            x_list.append(x)
+        x_concat = torch.cat(x_list, dim=-1)
+        if x.size(-1) == 1:
+            h_next = torch.relu(self.dc_initial(x_concat))
+        else:
+            h_next = torch.relu(self.dc_hidden(x_concat))
+        return h_next
+
+
+# Traffic Predictor
+class TrafficPredictor(nn.Module):
+    def __init__(self, node_features, hidden_channels, K):
+        super(TrafficPredictor, self).__init__()
+        self.dcrnn = DCRNNCell(node_features, hidden_channels, K)
+        self.linear = nn.Linear(hidden_channels, 1)
+
+    def forward(self, x_sequence, edge_index):
+        # x_sequence: [batch_size, seq_len, num_nodes, feature_dim]
+        batch_size, seq_length, num_nodes, _ = x_sequence.shape
+        outputs = []
+
+        for i in range(batch_size):
+            h = None
+            for t in range(seq_length):
+                h = self.dcrnn(x_sequence[i, t, :, :], edge_index)
+
+            predictions = []
+            for _ in range(4):  # predict next 4 timestamps
+                h = self.dcrnn(h, edge_index)
+                pred = self.linear(h)
+                predictions.append(pred)
+
+            outputs.append(torch.stack(predictions, dim=0))
+
+        return torch.stack(outputs, dim=0)
+
+# Normalized Laplacian Function
+
+def normalized_laplacian(edge_index, num_nodes):
+    A = to_dense_adj(edge_index).squeeze(0)
+    deg = degree(edge_index[0]).float()
+    D_inv_sqrt = torch.diag(1.0 / torch.sqrt(deg))
+    L = torch.eye(num_nodes) - torch.mm(torch.mm(D_inv_sqrt, A), D_inv_sqrt)
+    return L
+
+
+# Hyperparameters and Training
+
+if __name__ == '__main__':
+
+    hopsitalization = pd.read_csv('hosp_weekly_filt_case_data.csv')
+    hopsitalization = hopsitalization.reset_index()
+    hopsitalization['state'] = hopsitalization['state'].astype(int)
+
+    fips_list = hopsitalization['state'].values
+    fips_2_index = hopsitalization.set_index('state')['index'].to_dict()
+    index_2_fips = hopsitalization.set_index('index')['state'].to_dict()
+
+    neighbouring_states = pd.read_csv('neighbouring_states.csv')
+    states_code = pd.read_csv('state_hhs_map.csv')
+
+    neighbouring_states_copy = neighbouring_states[['NeighborStateCode', 'StateCode']]
+    neighbouring_states_copy.columns = ['StateCode', 'NeighborStateCode']
+
+    neighbouring_states = neighbouring_states.append(neighbouring_states_copy)
+    neighbouring_states.drop_duplicates()
+
+    fips_states = pd.read_csv('state_hhs_map.csv', header=None)
+    fips_2_state = fips_states.set_index(0)[2].to_dict()
+    state_2_fips = fips_states.set_index(2)[0].to_dict()
+    hopsitalization['state'] = hopsitalization['state'].map(fips_2_state)
+
+    index_2_state = hopsitalization.set_index('index')['state'].to_dict()
+    state_2_index = hopsitalization.set_index('state')['index'].to_dict()
+
+    neighbouring_states['StateCode'] = neighbouring_states['StateCode'].map(state_2_index)
+    neighbouring_states['NeighborStateCode'] = neighbouring_states['NeighborStateCode'].map(state_2_index)
+
+    G = nx.from_pandas_edgelist(neighbouring_states, 'StateCode', 'NeighborStateCode')
+    Pyg_graph = from_networkx(G)
+    edge_index = Pyg_graph.edge_index
+    hopsitalization = hopsitalization.iloc[:, 30:]  # remove all 0 datapoints
+    hopsitalization = hopsitalization.T.values
+
+    input_length = 12
+    output_length = 4
+    hidden_dim = 8
+
+    neighbouring_states_edges = neighbouring_states.iloc[0:109, :]
+
+    scaler = MinMaxScaler()
+    hopsitalization = scaler.fit_transform(hopsitalization)
+
+    node_features = 1
+    num_nodes = 49
+
+    length = hopsitalization.shape[0]
+    train_idx = int(length * 0.8)
+    val_idx = length - 4
+    # Split data into training, validation, and test sets
+    # [0-97] as training data, 98 timestamps, [98:118] as validation data, 21 timestamps , [102,122] total 21 timestamps
+    hopsitalization = np.expand_dims(hopsitalization, -1)
+    train_data = hopsitalization[:train_idx, :]
+    val_data = hopsitalization[train_idx:val_idx, :]
+    test_data = hopsitalization[(train_idx + 4):, :]
+
+    X_train, y_train = generate_sequences(train_data, input_length, output_length)
+    X_val, y_val = generate_sequences(val_data, input_length, output_length)
+    X_test, y_test = generate_sequences(test_data, input_length, output_length)
+
+    learning_rate = 0.001
+    hidden_dim = 8
+    K = 2
+    num_layers = 2
+    epochs = 100
+
+    model = TrafficPredictor(1, hidden_dim, K)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = nn.MSELoss()
+
+    for epoch in range(epochs):
+        model.train()
+        optimizer.zero_grad()
+        predictions_train = model(X_train, edge_index)  # [83, 4, 49, 1]
+        loss_train = criterion(predictions_train, y_train)
+        loss_train.backward()
+        optimizer.step()
+
+        model.eval()
+        with torch.no_grad():
+            predictions_val = model(X_val, edge_index)  # [6, 4, 49, 1]
+            loss_val = criterion(predictions_val, y_val)
+
+        print(
+            f"Epoch {epoch + 1}/{epochs} - Train Loss: {loss_train.item():.4f}, Validation Loss: {loss_val.item():.4f}")
